82b252ac2841c6d7d00572ae570789eea6541ee9
Valerie Tovar <vjtovar25@gmail.com>
2023-01-30 13:48:08 -0800

    add requirements

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import googlemaps
+from pprint import pprint
+
+load_dotenv() 
+api_key = os.getenv("API_KEY") 
+
+app = Flask(__name__)
+
+map_client = googlemaps.Client(api_key)
+
+starting_address = '1 Market st, San Francisco, CA'
+response = map_client.geocode(starting_address)
+# pprint(response)
+print(response[0]['geometry'])
+
+
+
+# """
+# Simple Program to help you get started with Google's APIs
+# """
+# import urllib.request, json
+# #Google MapsDdirections API endpoint
+# endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+# api_key = 'AIzaSyCLNO5mol_LjqDuOkTKLBke4Q9de-6GVy4'
+# #Asks the user to input Where they are and where they want to go.
+# origin = input('Where are you?: ').replace(' ','+')
+# destination = input('Where do you want to go?: ').replace(' ','+')
+# #Building the URL for the request
+# nav_request = 'origin={}&destination={}&key={}'.format(origin,destination,api_key)
+# request = endpoint + nav_request
+# #Sends the request and reads the response.
+# response = urllib.request.urlopen(request).read()
+# #Loads response as JSON
+# directions = json.loads(response)
+# print(directions)
+
+if __name__ == "__main__":
+    app.run(DEBUG=True)
+
+
