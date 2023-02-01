6daaad4dd64fefb965cff0c03cad7943c940d7e6
TStephens89 <treversion@yahoo.com>
2023-02-01 14:58:31 -0800

    100% covered

---
+++ static/script.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+
+//   $("#search").click(function() {
+//     var searchReq = $.get("/sendRequest/" + $("#query").val());
+//     searchReq.done(function(data) {
+//       $("#url").attr("href", data.result);
+//     });
+//   });
+
+// });
+// currentLocation = "disneyland"
+// destination = "universal"
+
+// var searchInput = "";
+// $("#search").click(function (event) {
+//   var currentLocation = document.getElementById("currentLocation");
+//   var destination = document.getElementById("destination");
+
+//   searchInput = value;
+//   event.preventDefault();
+
+//   makeCard();
--- test_app.py
+++ test_app.py
@@ -49,7 +49,18 @@ def test_invalid_directions(client):
     print(data)
     assert response.status_code == 404
 
+def test_file_exists():
+    assert os.path.exists('./app.py')
 
+
+def test_file_contents():
+    with open('./app.py', 'r') as f:
+        contents = f.read()
+    assert "api_key" in contents 
+
+def test_config():
+    assert not create_app('config').testing
+    assert create_app({'TESTING': True})
 
 
 
