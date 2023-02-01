526941201c3e674e7fa6e4aeb56ad9e6e6e6addd
jaolsen7 <jaolsen@berkeley.edu>
2023-02-01 09:11:30 -0800

    summary of trip

--- app.py
+++ app.py
@@ -14,17 +14,18 @@ gmaps = googlemaps.Client(key=api_key)
 def get_directions(x, y):
     directions_result = gmaps.directions(origin = x, destination = y)
     if directions_result:
-        return create_summary(directions_result), 200
+        return create_summary(directions_result, x, y), 200
     else:
         return json.dumps({"error": "Cannot find directions. Enter a drivable route!"}), 404
 
-def create_summary(directions):
+def create_summary(directions, origin, destination):
     distance = math.floor(directions[0]["legs"][0]["distance"]["value"]/1609.34)
     time = math.floor(directions[0]["legs"][0]["duration"]["value"]/3600)
     speed = distance/time
     endLatLon = directions[0]["legs"][0]["end_location"]
     startLatLon = directions[0]["legs"][0]["start_location"]
     modeOfTravel = directions[0]["legs"][0]["steps"][0]["travel_mode"]
+    summary = f"You will be starting at {origin} and {modeOfTravel.lower()} a total distance of {distance} miles with an average speed of {speed} mph until you reach your destination at {destination}."
 
     return json.dumps({"number of legs": len(directions[0]["legs"]),
                         "distance traveled": f"{distance} mi",
@@ -32,7 +33,8 @@ def create_summary(directions):
                         "average speed": f"{speed} mph",
                         "startLatLon": startLatLon,
                         "endLatLon": endLatLon,
-                        "travel mode": modeOfTravel
+                        "travel mode": modeOfTravel,
+                        "summary of trip": summary
     }, indent=4)
 
 if __name__ == "__main__":
