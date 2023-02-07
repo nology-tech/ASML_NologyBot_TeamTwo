af089f85d87c3610c8cc14ac5a632a699dad3a9a
Valerie Tovar <vjtovar25@gmail.com>
2023-02-06 20:13:17 -0800

    added logging and updated readme/test report

--- test_app.py
+++ test_app.py
@@ -1,21 +1,18 @@
+import logging
 import pytest
 from dotenv import load_dotenv
 import os
 from app import app, create_app
 import json
 from pytest import approx
-import logging
-from datetime import date
 
-
-today = date.today()
-logging.basicConfig(
-    filename='test_app.log', 
-    filemode='w',
-    level=logging.DEBUG, 
-    format="{asctime} {levelname:<8} {message}", 
-    style ='{'
-)
+log_format = '%(asctime)s   %(name)s    %(levelname)s    %(message)s'
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+file_handler = logging.FileHandler("test_app.log")
+formatter = logging.Formatter(log_format)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 load_dotenv() 
 api_key = os.getenv("API_KEY") 
@@ -36,13 +33,13 @@ def client(app):
 def runner(app):
     return app.test_cli_runner() 
 
-logging.info('Testing home route')
 def test_home_page(client):
+    logger.info('Testing home route')
     response = client.get('/')
     assert response.status_code == 200
 
-logging.info('Testing data types of decoded json object')
 def test_data_types(client):
+    logger.info('Testing data types of decoded json object')
     response = client.get('/directions/San_Antonio/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
@@ -50,8 +47,8 @@ def test_data_types(client):
     assert type(data) == dict
     assert response.status_code == 200 
 
-logging.info('Testing find legs')
 def test_find_legs(client):
+    logger.info('Testing find legs')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
@@ -59,8 +56,8 @@ def test_find_legs(client):
     assert data['number of legs'] == 1
     assert response.status_code == 200   
 
-logging.info('Testing find distance')
 def test_find_distance(client):
+    logger.info('Testing find distance')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
@@ -68,17 +65,17 @@ def test_find_distance(client):
     assert data['distance'] == approx("95 mi")
     assert response.status_code == 200
 
-logging.info('Testing find time')
 def test_find_time(client):
+    logger.info('Testing find time')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
-    assert data['time'] == approx("1 hrs")
     assert type(data['time']) == str
+    assert data['time'] == approx("1 hrs")
     assert response.status_code == 200
 
-logging.info('Testing find speed')
 def test_find_speed(client):
+    logger.info('Testing find speed')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
@@ -86,17 +83,17 @@ def test_find_speed(client):
     assert data['speed'] == approx('95 mph')
     assert response.status_code == 200
 
-logging.info('Testing find startlatLon')
 def test_find_startlatLon(client):
+    logger.info('Testing find startlatLon')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
     assert type(data['startLatLon']) == dict
     assert data['startLatLon'] == {"lat": 33.8160897,"lng": -117.9225226}
     assert response.status_code == 200
-
-logging.info('Testing find endlatLon')    
+   
 def test_find_endlatLon(client):
+    logger.info('Testing find endlatLon') 
     response = client.get('/directions/Disneyland/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
@@ -104,37 +101,38 @@ def test_find_endlatLon(client):
     assert data['endLatLon'] == {"lat": 32.7157323, "lng": -117.1610969}
     assert response.status_code == 200    
 
-logging.info('Testing Florida to San Diego travel mode')
 def test_valid_transportation(client):
+    logger.info('Testing Florida to San Diego travel mode')
     response = client.get('/directions/Florida/San_Diego/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
     assert data['travelModes'] == ['driving']
     assert response.status_code == 200
 
-logging.info('Testing invalid Disneyland to San Diego travel mode')
 def test_invalid_transportation(client):
+    logger.info('Testing Disneyland to San Diego invalid travel mode')
     response = client.get('/directions/Disneyland/San_Diego/driving')
     assert ['swimming'] not in ['travelModes']
     assert response.status_code == 200    
 
-logging.info('Testing invalid directions status code 404')
 def test_invalid_directions(client):
+    logger.info('Testing invalid directions status code 404')
     response = client.get('/directions/Hawaii/San_Diego/driving')
     assert b"An error occurred or there are no available directions for this search." in response.data
     assert response.status_code == 404
 
-logging.info('Testing Seaworld to Los Angeles driving mode')
 def test_driving_mode(client):
+    logger.info('Testing Seaworld to Los Angeles driving mode')
     response = client.get('/directions/SeaWorld/Los_Angeles/driving')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)
+    assert type(data['travelModes']) == list
     assert len(data['travelModes']) == 1
     assert 'driving' in data['travelModes']
     assert response.status_code == 200
 
-logging.info('Testing Seaworld to Los Angeles transit mode')
 def test_transit_mode(client):
+    logger.info('Testing Seaworld to Los Angeles transit mode')
     response = client.get('/directions/SeaWorld/Los_Angeles/transit')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)  
@@ -143,26 +141,40 @@ def test_transit_mode(client):
     assert 'transit' and 'walking' in data['travelModes'] 
     assert response.status_code == 200
 
-logging.info('Testing summary data of directions')
-def test_summary_data(client):
+def test_summary_data_driving(client):
+    logger.info('Testing summary of driving directions')
+    response = client.get('/directions/SeaWorld/Los_Angeles/driving')
+    my_json = response.data.decode("UTF-8")
+    data = json.loads(my_json)  
+    assert type(data['summary']) == str
+    speed = approx(data['speed'])
+    distance = approx(data['distance'])
+    assert data['summary'] == f"You will be starting at SeaWorld and driving a total distance of {distance} with an average speed of {speed} until you reach your destination at Los_Angeles."
+
+def test_summary_data_transit(client):
+    logger.info('Testing summary of transit directions')
     response = client.get('/directions/SeaWorld/Los_Angeles/transit')
     my_json = response.data.decode("UTF-8")
     data = json.loads(my_json)  
     assert type(data['summary']) == str
-    assert data['summary'] == "You will be starting at SeaWorld and walking/taking public transit a total distance of 2690 mi with an average speed of 44 mph until you reach your destination at Los_Angeles."
+    speed = approx(data['speed'])
+    distance = approx(data['distance'])
+    assert data['summary'] == f"You will be starting at SeaWorld and walking/taking public transit a total distance of {distance} with an average speed of {speed} until you reach your destination at Los_Angeles."
     assert response.status_code == 200    
 
-logging.info('Testing file path to app exists')      
 def test_file_exists():
+    logger.info('Testing file path to app exists') 
     assert os.path.exists('./app.py')
 
-logging.info('Testing file contents of app')
 def test_file_contents():
+    logger.info('Testing file contents of app')
     with open('./app.py', 'r') as f:
         contents = f.read()
     assert "api_key" in contents 
 
-logging.info('Testing config of app')
 def test_config():
+    logger.info('Testing config of app')
     assert not create_app('config').testing
     assert create_app({'TESTING': True})
+
+
